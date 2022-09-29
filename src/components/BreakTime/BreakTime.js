6d1a709c09ba60ef3b39af8328a2739d8d7ec1f7
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-09-29 12:07:35 +0600

    added style break time btn

--- src/components/BreakTime/BreakTime.js
+++ src/components/BreakTime/BreakTime.js
@@ -4,12 +4,12 @@ const BreakTime = ({handleBreakTime}) => {
   return (
     <div>
       <h5>Add A Break</h5>
-      <div className='bg-slate-300 px-3 py-3 rounded-lg'>
-        <button onClick={() => handleBreakTime(10)} className='rounded-md bg-orange-200 ml-2'>10s</button>
-        <button onClick={() => handleBreakTime(20)} className='rounded-md bg-orange-200 ml-2'>20s</button>
-        <button onClick={() => handleBreakTime(30)} className='rounded-md bg-orange-200 ml-2'>30s</button>
-        <button onClick={() => handleBreakTime(40)} className='rounded-md bg-orange-200 ml-2'>40s</button>
-        <button onClick={() => handleBreakTime(50)} className='rounded-md bg-orange-200 ml-2'>50s</button>
+      <div className='bg-slate-300 px-3 py-3 rounded-lg text-center'>
+          <button onClick={() => handleBreakTime(10)} className='rounded-full p-2 bg-orange-400 ml-2'>10s</button>
+          <button onClick={() => handleBreakTime(20)} className='rounded-full p-2 bg-orange-400 ml-2'>20s</button>
+          <button onClick={() => handleBreakTime(30)} className='rounded-full p-2 bg-orange-400 ml-2'>30s</button>
+          <button onClick={() => handleBreakTime(40)} className='rounded-full p-2 bg-orange-400 ml-2'>40s</button>
+          <button onClick={() => handleBreakTime(50)} className='rounded-full p-2 bg-orange-400 ml-2'>50s</button>
       </div>
     </div>
   );
