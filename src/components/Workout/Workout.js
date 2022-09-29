9122a4e5b04e5845863f751c562c67dcad9ddf6e
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-09-29 12:43:31 +0600

    added style card

--- src/components/Workout/Workout.js
+++ src/components/Workout/Workout.js
@@ -4,14 +4,14 @@ import "./Workout.css";
 const Workout = ({ workout, handleToDoList }) => {
   const { id, name, img, age, time, about } = workout;
   return (
-    <div className="card p-3">
+    <div className="card p-3 bg-white shadow-md rounded-md">
       <img className="h-56 w-fit mx-auto rounded-md" src={img} alt="" />
       <h5 className="text-xl font-bold mb-2 mt-2">{name}</h5>
       <p className="text-gray-400 mb-2">{about}</p>
       <p>For Age: {age} </p>
       <p>Time Require: {time}s </p>
       <button
-        className="bg-lime-500 w-full rounded-md py-2 mt-2"
+        className="bg-lime-500 w-full rounded-md py-2 mt-4"
         onClick={() => {
           handleToDoList(id);
         }}
