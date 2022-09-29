d7e031a79ecdc9a883421a88f7f1a63cf59ecea8
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-09-29 11:14:19 +0600

    added style to workout component and Exercise component

--- src/components/Exercise/Exercise.js
+++ src/components/Exercise/Exercise.js
@@ -19,20 +19,30 @@ const Exercise = () => {
   };
 
   return (
-    <div className="grid grid-cols-3">
-      <div className="col-span-2">
-        <div className="grid grid-cols-3 gap-4">
-          {exercise.map((workout) => (
-            <Workout
-              key={workout.id}
-              workout={workout}
-              handleToDoList={handleToDoList}
-            ></Workout>
-          ))}
+    <div className="bg-zinc-50">
+      <div className="grid grid-cols-3">
+        <div className="col-span-2 p-11">
+          <div>
+            <h1 className="uppercase font-extrabold text-3xl text-stone-400">
+              Power Surge Gym
+            </h1>
+            <p className="text-lg font-medium text-stone-600 mt-3 mb-3">
+              Select Today's Exercise
+            </p>
+          </div>
+          <div className="grid grid-cols-3 gap-4">
+            {exercise.map((workout) => (
+              <Workout
+                key={workout.id}
+                workout={workout}
+                handleToDoList={handleToDoList}
+              ></Workout>
+            ))}
+          </div>
         </div>
-      </div>
-      <div className="">
-        <Sidebar toDoList={toDoList}></Sidebar>
+        <div className="">
+          <Sidebar toDoList={toDoList}></Sidebar>
+        </div>
       </div>
     </div>
   );
--- src/components/Workout/Workout.js
+++ src/components/Workout/Workout.js
@@ -1,16 +1,21 @@
-import React from 'react';
-import './Workout.css';
+import React from "react";
+import "./Workout.css";
 
-const Workout = ({workout, handleToDoList}) => {
-  const {id,name, img, age, time, about,} = workout;
+const Workout = ({ workout, handleToDoList }) => {
+  const { id, name, img, age, time, about } = workout;
   return (
-    <div className='card p-3'>
-      <img className='h-56 w-fit mx-auto rounded-md' src={img} alt="" />
-      <h5 className='text-xl font-bold mb-2 mt-2'>{name}</h5>
-      <p className='text-gray-400 mb-2'>{about}</p>
+    <div className="card p-3">
+      <img className="h-56 w-fit mx-auto rounded-md" src={img} alt="" />
+      <h5 className="text-xl font-bold mb-2 mt-2">{name}</h5>
+      <p className="text-gray-400 mb-2">{about}</p>
       <p>For Age: {age} </p>
       <p>Time Require: {time}s </p>
-      <button className='bg-lime-500 w-full rounded-md py-2 mt-2' onClick={() => {handleToDoList(id)}}>
+      <button
+        className="bg-lime-500 w-full rounded-md py-2 mt-2"
+        onClick={() => {
+          handleToDoList(id);
+        }}
+      >
         <p>Add to list</p>
       </button>
     </div>
