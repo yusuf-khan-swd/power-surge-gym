149977fdbc524436957e22ab997157a8324a5dbe
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-09-29 09:09:35 +0600

    get data from localStorage and set the value

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import BreakTime from '../BreakTime/BreakTime';
 
 const Sidebar = ({toDoList}) => {
@@ -10,9 +10,17 @@ const Sidebar = ({toDoList}) => {
   }
 
   const handleBreakTime = breakTime => {
-    localStorage.setItem('break-time', breakTime.toString());
     setBreakTime(breakTime);
+    localStorage.setItem('break-time', JSON.stringify(breakTime));
   }
+
+  useEffect(() => {
+    const savedBreakTime = localStorage.getItem('break-time');
+    if(savedBreakTime) {
+      setBreakTime(savedBreakTime)
+    }
+  }, []);
+
   return (
     <div>
       <BreakTime handleBreakTime={handleBreakTime}></BreakTime>
